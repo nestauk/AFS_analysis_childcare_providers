be2def68aca3c334a0c42c2bb1390e0dcbf2324e
Izzy Stewart <isabelrstewart@gmail.com>
2021-06-25 13:17:52 +0100

    Setup Nesta Data Science cookiecutter (#1)
    Co-authored-by: Isabel <isabel.stewart@nesta.org.uk>

---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. toctree::
+   :hidden:
+   :maxdepth: 1
+
+   api/afs_analysis_childcare_providers
+   License <license>
+   Github <https://github.com/nestauk/AFS_analysis_childcare_providers/>
+
+AFS_analysis_childcare_providers
+===============================
+
+Data analysis work around childcare providers takeup and the effect of covid on school readiness for early years.
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,26 @@
+"""afs_analysis_childcare_providers."""
+from pathlib import Path
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read_lines(path):
+    """Read lines of `path`."""
+    with open(path) as f:
+        return f.read().splitlines()
+
+
+BASE_DIR = Path(__file__).parent
+
+
+setup(
+    name="afs_analysis_childcare_providers",
+    long_description=open(BASE_DIR / "README.md").read(),
+    install_requires=read_lines(BASE_DIR / "requirements.txt"),
+    extras_require={"dev": read_lines(BASE_DIR / "requirements_dev.txt")},
+    packages=find_packages(exclude=["docs"]),
+    version="0.1.0",
+    description="Data analysis work around childcare providers takeup and the effect of covid on school readiness for early years.",
+    author="Nesta",
+    license="MIT",
+)
